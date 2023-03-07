c720efb85f2bb96d0947fa6ed5ebb49cef2174b4
Natali <natali.dnp.ua@gmail.com>
2023-03-07 23:55:38 +0200

    add get_context_data to RoomView(TemplateView)

--- apps/game/views.py
+++ apps/game/views.py
@@ -18,12 +18,25 @@ def index(request):
 
 class RoomListView(ListView):
     model = Room
+    queryset = Room.objects.all()
 
 
 class RoomView(TemplateView):
     model = Word
     success_url = reverse_lazy("game:list")
     template_name = "game/word_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        room_id = context["pk"]
+
+        room = Room.objects.filter(pk=room_id).get()
+        context["title"] = room.name
+        words = room.word_set.all()
+        # words = Word.objects.all()
+        context["words"] = words
+
+        return context
 
 
 class WordCreateView(CreateView):
